d84cce10e6f3d4c691b02d60cc14898c9911bd89
essennejaye <sjeffers02@gmail.com>
2020-08-30 22:25:53 -0700

    homepage view added

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,15 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// create connection to our database, pass in MySQL information for username and password
-// connect to heroku database if deployed
+// create connection to our database, pass in MySQL information from .env for username and password
+// connect to local database or heroku database if deployed
 let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+sequelize = process.env.JAWSDB_URL
+  ? sequelize = new Sequelize(process.env.JAWSDB_URL)
+  : sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
-}
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,24 +1,15 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+// const sequelize = require('../config/connection');
 const { Posts, User, Comments } = require('../models');
 
 router.get('/', (req, res) => {
   Posts.findAll({
     attributes: [
-      'id',
       'title',
       'posts_text',
       'created_at'
     ],
     include: [
-      {
-        model: Comments,
-        attributes: ['id', 'comments_text', 'posts_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
       {
         model: User,
         attributes: ['username']
